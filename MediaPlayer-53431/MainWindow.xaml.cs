78610639c31f5ebeee69851e43fc6cbbe88bee18
kkaterynaol <kkaterynaol@gmail.com>
2016-06-23 05:58:09 +0200

    MediaPlayer-1

---
+++ MediaPlayer-53431/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Microsoft.Win32;
+
+namespace MediaPlayer.Audio_and_Video
+{
+    public partial class AudioVideoPlayerCompleteSample : Window
+    {
+
+    }
+}
