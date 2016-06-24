e0407acde4699c0568b7b112bd0fb22d4c96ed24
kkaterynaol <kkaterynaol@gmail.com>
2016-06-24 09:11:49 +0200

    Media Player - 3

--- MediaPlayer-53431/MainWindow.xaml.cs
+++ MediaPlayer-53431/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Threading;
 using Microsoft.Win32;
 
 namespace MediaPlayer.Audio_and_Video
+
 {
     public partial class AudioVideoPlayerCompleteSample : Window
     {
@@ -22,6 +23,13 @@ namespace MediaPlayer.Audio_and_Video
             timer.Start();
         }
 
+        /// <summary>
+        /// Timer, ktory liczy czas odtwarzania pliku multimedialnego
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
         private void timer_Tick(object sender, EventArgs e)
         {
             if ((mePlayer.Source != null) && (mePlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
@@ -32,11 +40,25 @@ namespace MediaPlayer.Audio_and_Video
             }
         }
 
+        /// <summary>
+        /// Sprawdza, czy moze otworzyc okno i czy nie ma bledow
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
         private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
         }
 
+        /// <summary>
+        /// Otwiera okno dla pobierania pliku multimedialnego
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
         private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -45,38 +67,128 @@ namespace MediaPlayer.Audio_and_Video
                 mePlayer.Source = new Uri(openFileDialog.FileName);
         }
 
+        /// <summary>
+        /// Metoda, ktora sprawdza, czy okno playera nie jest puste i czy zostal wczytany plik
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
         private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = (mePlayer != null) && (mePlayer.Source != null);
         }
 
+        /// <summary>
+        /// Metoda, za pomoca ktorej player zaczyna odtwarzac plik
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
         private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             mePlayer.Play();
             mediaPlayerIsPlaying = true;
         }
 
+        /// <summary>
+        /// Metoda sprawdza, czy plik jest nagrywany i czy mozna go wstrzymac
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
         private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = mediaPlayerIsPlaying;
         }
 
+        /// <summary>
+        /// Metoda wstrzyma odtwarzanie pliku
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
         private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             mePlayer.Pause();
         }
 
+        /// <summary>
+        /// Metoda sprawdza, czy plik moze byc zatrzymany
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
         private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = mediaPlayerIsPlaying;
         }
 
+        /// <summary>
+        /// Metoda zatrzymuje odtwarzanie pliku
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
         private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             mePlayer.Stop();
             mediaPlayerIsPlaying = false;
         }
 
+        /// <summary>
+        /// Metoda steruje dzialaniem suwaka, zeby zmieniac czas i ogladac lub sluchac plik z dowolnego miejsca
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
+        private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+        /// <summary>
+        /// Metoda steruje dzialaniem suwaka, zeby zmieniac czas i ogladac lub sluchac plik z dowolnego miejsca
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
+        private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            mePlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
+        }
+
+        /// <summary>
+        /// Metoda steruje dzialaniem suwaka, zeby zmieniac czas i ogladac lub sluchac plik z dowolnego miejsca
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
+        private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
+        }
+
+        /// <summary>
+        /// Metoda zmienia glosnosc odtwarzania
+        /// </summary>
+        /// <permission>Private</permission>
+        /// <param name="sender">default value</param>
+        /// <param name="e">default value</param>
+        /// <returns>Void</returns>
+        private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            mePlayer.Volume += (e.Delta > 0) ? 0.1 : -0.1;
+        }
 
 
     }
---
+++ MediaPlayerTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MediaPlayerTest
+{
+    [TestClass]
+    public class MediaPlayerTest
+    {
+        [TestMethod]
+        public void Play_CanExecutedTest()
+        {
+
+        }
+        [TestMethod]
+        public void Pause_CanExecutedTest()
+        {
+
+        }
+        [TestMethod]
+        public void Stop_CanExecutedTest()
+        {
+
+        }
+        [TestMethod]
+        public void sliProgress_DragCompleteTest()
+        {
+
+        }
+        [TestMethod]
+        public void Grid_MouseWheeTest()
+        {
+
+        }
+
+    }
+}
